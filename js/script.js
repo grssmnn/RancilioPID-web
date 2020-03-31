29c8c077bb12e404cde65318e1e4153c0a38bfc6
Kevin Großmann <kevingrossmann@Kevins-MacBook-Pro.local>
2020-03-31 17:01:10 +0200

    Added graphs for pid values

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
 $(function() {
+    /**
+     * Init Data from Local Storage
+     */
     var temperatureData = localStorage.getItem("temperatureData") == null ? [] : JSON.parse(localStorage.getItem('temperatureData'));
     var controlTemperatureData = localStorage.getItem("controlTemperatureData") == null ? [] : JSON.parse(localStorage.getItem('controlTemperatureData'));
+    var pidKpData = localStorage.getItem("pidKpData") == null ? [] : JSON.parse(localStorage.getItem('pidKpData'));
+    var pidKiData = localStorage.getItem("pidKiData") == null ? [] : JSON.parse(localStorage.getItem('pidKiData'));
+    var pidKdData = localStorage.getItem("pidKdData") == null ? [] : JSON.parse(localStorage.getItem('pidKdData'));
 
-
-    console.log(temperatureData);
-
-    var now = (new Date()).getTime();
-    while (temperatureData.length > 0) {
-        var time = Date.parse(temperatureData[0].x);
-        if ((now - time) > 60 * 1000) {
-            temperatureData.shift();
-            controlTemperatureData.shift();
-        } else
-            break;
-    }
-
+    /**
+     * Init Chart
+     */
     var ctx = document.getElementById('chart').getContext('2d');
     var data = {
         "datasets": [{
@@ -22,24 +18,66 @@ $(function() {
                 "backgroundColor": "rgba(81, 196, 197,0.3)",
                 "borderColor": "rgb(81, 196, 197)",
                 "fill": false,
-                "data": temperatureData
+                "data": temperatureData,
+                yAxisID: 'tempAxis',
             },
             {
                 "label": "Soll Temperatur",
                 "backgroundColor": "rgba(0,0,0,0.3)",
                 "borderColor": "#000",
                 "fill": false,
-                "data": controlTemperatureData
-            }
+                "data": controlTemperatureData,
+                yAxisID: 'tempAxis',
+            },
+            {
+                "label": "P",
+                "backgroundColor": "rgba(23,55,83,0.3)",
+                "borderColor": "rgba(23,55,83,0.3)",
+                "fill": false,
+                "data": pidKpData,
+                steppedLine: true,
+                yAxisID: 'pidAxis',
+                hidden: true,
+            },
+            {
+                "label": "I",
+                "backgroundColor": "rgba(40,146,215,0.3)",
+                "borderColor": "rgba(40,146,215,0.3)",
+                "fill": false,
+                "data": pidKiData,
+                steppedLine: true,
+                yAxisID: 'pidAxis',
+                hidden: true,
+            },
+            {
+                "label": "D",
+                "backgroundColor": "rgba(109,174,219,0.5)",
+                "borderColor": "rgba(109,174,219,0.5)",
+                "fill": false,
+                "data": pidKdData,
+                steppedLine: true,
+                yAxisID: 'pidAxis',
+                hidden: true,
+            },
         ]
     };
     var options = {
         "scales": {
             yAxes: [{
-                "ticks": {
-                    "beginAtZero": true
+                    id: 'tempAxis',
+                    position: 'left',
+                    ticks: {
+                        "beginAtZero": true
+                    }
+                },
+                {
+                    id: 'pidAxis',
+                    position: 'right',
+                    ticks: {
+                        beginAtZero: true
+                    }
                 }
-            }],
+            ],
             xAxes: [{
                 type: 'time',
                 time: {
@@ -48,7 +86,6 @@ $(function() {
             }]
         }
     };
-
     var myChart = new Chart(ctx, {
         type: 'line',
         data: data,
@@ -57,8 +94,10 @@ $(function() {
 
 
 
+    /**
+     * Add randomly dummy data
+     */
     var last = 100;
-
     setInterval(function() {
         var date = new Date();
         var a = {
@@ -69,33 +108,62 @@ $(function() {
             x: date,
             y: 100
         };
+        var p = {
+            x: date,
+            y: getRandomInt(10) + 10
+        }
+        var i = {
+            x: date,
+            y: getRandomInt(10) + 10
+        }
+        var d = {
+            x: date,
+            y: getRandomInt(10) + 10
+        }
 
         myChart.data["datasets"][0].data.push(a);
         myChart.data["datasets"][1].data.push(b);
 
+        myChart.data["datasets"][2].data.push(p);
+        myChart.data["datasets"][3].data.push(i);
+        myChart.data["datasets"][4].data.push(d);
+
+
         localStorage.setItem("temperatureData", JSON.stringify(temperatureData));
         localStorage.setItem("controlTemperatureData", JSON.stringify(controlTemperatureData));
-        console.log(myChart.data["datasets"][1].data);
+        localStorage.setItem("pidKpData", JSON.stringify(pidKpData));
+        localStorage.setItem("pidKiData", JSON.stringify(pidKiData));
+        localStorage.setItem("pidKdData", JSON.stringify(pidKdData));
+
+        removeOldEntries(60);
+
         myChart.update();
-        //addData(myChart, a, b);
     }, 3000);
 
+    /**
+     * Returns random int number for dummy data.
+     * @param {int} max 
+     */
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
     }
 
-
-
-    function addData(chart, a, b) {
-        temperatureData.push(a);
-        controlTemperatureData.push(b);
-
-        localStorage.setItem("temperatureData", JSON.stringify(temperatureData));
-        localStorage.setItem("controlTemperatureData", JSON.stringify(controlTemperatureData));
-
-        chart.data[0].push(a);
-        chart.data[1].push(b);
-
-        chart.update();
+    /**
+     * Removes entries which are older than x seconds.
+     * @param {int} seconds
+     */
+    function removeOldEntries(seconds) {
+        var now = (new Date()).getTime();
+        while (temperatureData.length > 0) {
+            var time = Date.parse(temperatureData[0].x);
+            if ((now - time) > seconds * 1000) {
+                temperatureData.shift();
+                controlTemperatureData.shift();
+                pidKpData.shift();
+                pidKiData.shift();
+                pidKdData.shift();
+            } else
+                break;
+        }
     }
 });
